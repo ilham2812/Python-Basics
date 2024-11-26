30da1cf56b289853c359e888a62b3606ea3bcb18
Ilham <120342782+ilham2812@users.noreply.github.com>
2024-11-26 23:24:05 +0100

    Add files via upload

---
+++ First.py
@@ -0,0 +1,14 @@
+print("Programming is like building a multilingual puzzele")
+
+number = 98 
+print (f"{number} battery street")
+
+
+number = 3.14159 
+print (f" Float :{number} ")
+
+
+str = "Ilhamo"
+print ((str))
+print ((str))
+print ((str))
