ffbd9d438961c3bfab0d6a39123d06d44f8effbb
Ilham <120342782+ilham2812@users.noreply.github.com>
2024-11-27 23:44:03 +0100

    Add files via upload

---
+++ Tr.py
@@ -0,0 +1,15 @@
+# For Loop
+rows = 8
+for i in range(1, rows + 1):
+    print("*" * i)
+
+
+
+# While Loop
+x = 0
+y = 8
+while x != y:
+    print(f"x: {x}")
+    x += 1  
+print(f"x: {x} , Now x = y ")
+
